c95c694db28232ea20f193aa5275d3d182177661
Christoph <christoph@mixxt.net>
2013-11-26 13:31:29 +0100

    update to 0.2-alpha, add examples

--- js/debug.js
+++ js/debug.js
@@ -1,11 +1,8 @@
 $.fn.enableMyDebug = function(){
     return $(this).each(function(){
         var $el = $(this),
-            instance = $el.data('mentionsKinder'),
-            $debugArea = $('<textarea class="debug-area form-control" readonly name="'+ instance.$input.attr('name') +'"></textarea>');
+            instance = $el.data('mentionsKinder');
 
-        instance.$input.replaceWith($debugArea);
-        $debugArea.val(instance.$input.val());
-        instance.$input = $debugArea;
+        instance.$originalInput.removeClass('mentions-kinder-hidden').addClass('debug-area form-control').attr('readonly', true);
     });
 };
